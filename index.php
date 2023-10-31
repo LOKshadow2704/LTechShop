3b0d6cc671bcd563fe68e4df3c43f94a9359232d
LOKshadow2704 <nguyenlocface@gmail.com>
2023-10-31 14:55:47 +0700

    a

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div id="root">
+        <header>
+            <div class="info">
+                <div class="logo">
+                    <img src="" alt="">
+                </div>
+                <div class="search">
+                    <input type="text" placeholder="Tìm kiếm">
+                </div>
+            </div>
+            <nav>
+
+            </nav>
+        </header>
+        <div class="container">
+            <div class="top_save">
+
+            </div>
+            <div class="sale_off">
+
+            </div>
+        </div>
+        <div class="footer">
+
+        </div>
+    </div>
+</body>
+</html>
