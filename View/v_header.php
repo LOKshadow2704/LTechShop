d4fab775c9a50cf0355a141c178ea279b03b2145
LOKshadow2704 <nguyenlocface@gmail.com>
2023-11-02 17:57:40 +0700

    a

---
+++ View/v_header.php
@@ -0,0 +1,36 @@
+<?php
+    echo "<header>
+    <div class='top_header'>
+        <div class='logo'>
+            <img src='https://doc-0g-6c-docs.googleusercontent.com/docs/securesc/73fok75ijjgjhu3v2i2tqhjlljmjfkgg/7i4bbsm4klnjq77aomjf13fjveuqp9il/1698846825000/02860064968007549767/02860064968007549767/1cupgxHCAWt0pOn4tUXwBsE-qRN_15Fzu?ax=AI0foUpjwj5wCGXcRNh9FgSzANfSJbTnCIzUkwmUGEdnrerFZ39hx32Pqe2mc9lIrKLXcq5c4Phm1F3rTR_vXe5xWefLEnTj12PTdNOw1SdpJ-kmiF8BLEwMKDlShnK68EHL8xUFZG9sM3e2ZweJzl77tjT2Hhm7Z6Ck-W3kUf3HjlO5_Eo4GxQrMgh_VSLw60iA-JF3OXxRunRHqF7FexToPaGr-pJwmti0JB4JWQq_M1LDbnR73IY5CVYkGxXsY0Z8xqmAkpoEfPP3DvAAPlxJO2XPhPk17O2W8rqTjojfzILgVGwms4eUXBXfpRWeY-fpH84Xk6WLtLLOfySDoLkAm13Ix3_lPNVLSd9vh1kuLbJe9ztbgJk6OybOfK18zF9eopUGfpP3C9K0lEY-D_6GEG2yIfYKUAAAds5oGz9rHs050dTfW1dmPD5KkAUMHAOnJjFLAu8rgxWfjWYgMC24R0Hi9aLLowSAUPK-Q4iWTvV2eqN4v5EDXhIJLUt8FmmpHIQJZQ6vkI4Flrbx_0EeWi1m99rWs9juA6pPFjBTpqXJ4OgRnE8bad9zf_xzrjAqIf1jSLoUk9c7LWKzjWcE9agM0tS3FmyCVCMLF8Iy9QfXGQnJ_MTBdHO0IcQRwM6Ld5eQrU-1XlzOkedirGpsIuvZGSur1XwcH905qH8nWMtpbzJc5aTCV9zHW66cqe76TEbQDltvcX6xIzlnaOu7pjLkI2ZKSY6BhifySJBq2dGV-gd-0i1nyWc6UkIBtVQrhgM3ZvZef41slTwxyNg1e_oiVvttpR0yR5SJKdBNyNPUfOh_HPskX40Evl4nItQUkDUam-NKo5rVwwum7lmhgZDJwqOw2QvpOG2YOO-fa79rEoj6W10IEjEsW_Zgbas&uuid=af650569-fa72-43a6-a357-9d35472c2994&authuser=0&nonce=ujq9392fb3i9s&user=02860064968007549767&hash=5jsais18716ke59hb92l2cid7dpek8v5' alt='' width='100%' height='100%'>
+        </div>
+        <div class='search'>
+            <input type='text' placeholder='Tìm kiếm'>
+            <i class='fa-solid fa-magnifying-glass'></i>
+        </div>
+        <div class='top_right_header'>
+            <div class='account'>
+                <a href=''>
+                    <i class='fa-solid fa-user' style='color: #ffffff;'></i>
+                </a>
+                <a href=''>Đăng nhập</a><p>|</p><a href=''>Đăng ký</a>
+            </div>
+            <div class='cart'>
+                <a href=''>
+                    <i class='fa-solid fa-cart-plus' style='color: #ffffff;'></i>
+                    Giỏ hàng
+                </a>
+            </div>
+        </div>
+    </div>
+    <nav>
+        <ul>
+            <li> <a href=''>Trang chủ</li></a>
+            <li> <a href=''>Quản lý sản phẩm của bạn</li></a>
+            <li> <a href=''>Sản phẩm</li></a>
+            <li> <a href=''>Đơn bán</li></a>
+            <li> <a href=''>Đơn mua</li></a>
+        </ul>
+    </nav>
+</header>";
+?>
